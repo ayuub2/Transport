c5b274c65223737e70b65244bb35fbb4e6e2c423
ayuub2 <ayuubadan@outlook.com>
2016-10-26 14:33:39 +0100

    Revert "random"
    This reverts commit 71b56b81e62c744f6e6fa45683e8b57804c808c6.

--- src/js/pages/content.jsx
+++ src/js/pages/content.jsx
@@ -1,19 +1,57 @@
 var React = require("-aek/react");
 var {BasicSegment} = require("-components/segment");
+var request = require("@ombiel/aek-lib/request");
 var Bus = require("./bus");
 var Train = require("./train");
 var Timetable = require("./timetable");
 var Time = require("./time");
-var DataPage = require("./data");
+var busNumber = ["61","62","74","76","87"];
+
+
 
 var ContentPage = React.createClass({
 
+  getInitialState: function(){
+
+    return {};
+  },
+
+  componentDidMount: function(){this.getData();},
+
+  getData: function(){
+    request
+    .get('http://transportapi.com/v3/uk/bus/stop/1500IM2468/live.json?')
+    .query({
+      app_key:"87fa98daa26fb2c5077fb8bdd0bd0264",
+      app_id:"62561a7e"
+    })
+    .end((err, res)=>{
+      this.setState({transport:res.body});
+    });
+  },
+
   render: function (){
     var page = this.props.page;
+    var transport = this.state.transport;
+    var busObject = [];
+    var allBuses = [];
+
+    if(transport !== undefined){
+      var list = transport.departures;
+      for(var i in busNumber){
+        busObject.push(list[busNumber[i]]);
+      }
+      for (var j in busObject){
+        var length = busObject[j].length;
+        for(var x = 0; x < length; x++){
+          allBuses.push(busObject[j][x.toString()]);
+        }
+      }
+      console.log(allBuses);
+    }
     var contentDisplayed;
-    var data = DataPage.sendData()
     if(page === "bus"){
-      contentDisplayed = <Bus buses={da}/>;
+      contentDisplayed = <Bus buses={allBuses}/>;
     }
     else if(page === "train"){
       contentDisplayed = <Train/>;
@@ -21,7 +59,6 @@ var ContentPage = React.createClass({
     else {
       contentDisplayed = <Timetable/>;
     }
-    dsadas
     return (
       <BasicSegment>
         <BasicSegment>
