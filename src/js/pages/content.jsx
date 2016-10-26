fe01db4e94e3d2d5dc520b231c6dbc5986b994a2
ayuub2 <ayuubadan@outlook.com>
2016-10-26 11:40:07 +0100

    retrieved all buses

--- src/js/pages/content.jsx
+++ src/js/pages/content.jsx
@@ -1,18 +1,55 @@
 var React = require("-aek/react");
 var {BasicSegment} = require("-components/segment");
-var Button = require("@ombiel/aek-lib/react/components/button");
+var request = require("@ombiel/aek-lib/request");
 var Bus = require("./bus");
 var Train = require("./train");
 var Timetable = require("./timetable");
 var Time = require("./time");
+var busNumber = ["61","62","74","76","87"];
+var busObject = [];
+var allBuses = [];
+
+
 
 var ContentPage = React.createClass({
 
+  getInitialState: function(){
+
+    return {};
+  },
+
+  componentDidMount: function(){this.getData();},
+
+  getData: function(){
+    request
+    .get('http://transportapi.com/v3/uk/bus/stop/1500IM2468/live.json?')
+    .query({
+      app_key:"87fa98daa26fb2c5077fb8bdd0bd0264",
+      app_id:"62561a7e"
+    })
+    .end((err, res)=>{
+      this.setState({transport:res.body});
+    });
+  },
+
   render: function (){
     var page = this.props.page;
+    var transport = this.state.transport;
+    if(transport !== undefined){
+      var list = transport.departures;
+      for(var i in busNumber){
+        busObject.push(list[busNumber[i]]);
+      }
+      for (var j in busObject){
+        var length = busObject[j].length;
+        for(var x = 0; x < length; x++){
+          allBuses.push(busObject[j][x.toString()]);
+        }
+      }
+    }
     var contentDisplayed;
     if(page === "bus"){
-      contentDisplayed = <Bus/>;
+      contentDisplayed = <Bus buses={allBuses}/>;
     }
     else if(page === "train"){
       contentDisplayed = <Train/>;
